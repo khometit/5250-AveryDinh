c46f82e916a0785b36bd71f10b453a7e0a0f67c5
Avery Dinh <averydinh1996@gmail.com>
2022-01-14 11:21:32 -0800

    Added comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,18 @@
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Model to create items for the characters to use and Monsters to drop
+    /// </summary>
     public class ItemModel
     {
+        //The ID for the item
         public string Id { get; set; }
+
+        //The Display Text for the item
         public string Text { get; set; }
+
+        //The Description of the item
         public string Description { get; set; }
 
         //The value of the Item +9 Damage
