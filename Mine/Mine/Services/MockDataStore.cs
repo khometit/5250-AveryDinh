e81cf5cec957c7d592f805a651bd82a6973a4f2b
Avery Dinh <averydinh1996@gmail.com>
2022-01-14 12:57:26 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Cottage Ball", Description="Hit you with softness.", Value=7 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Holy light", Description="Bright and holy like your future.", Value=10 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Electric kettle", Description="Spice up your morning with some hot tea.", Value=5 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Destroyer's comb", Description="Coming at your hair to make them extra pretty.", Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Keyboard", Description="Help you express yourself digitally.", Value=4 },
             };
         }
 
