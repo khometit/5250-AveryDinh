7e4f0b95c78df6af9e918bb0ccb553f7c9536711
Avery Dinh <averydinh1996@gmail.com>
2022-01-21 22:43:52 -0800

    Added Toolbar to Update page and event method in ItemReadPage.xaml, ItemReadPage.xaml.cs

--- Mine/Mine/Views/Items/ItemReadPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemReadPage.xaml.cs
@@ -49,5 +49,12 @@ namespace Mine.Views
 
             await Navigation.PopAsync();
         }
+
+        public async void UpdateItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ItemUpdatePage(viewModel)));
+
+            await Navigation.PopAsync();
+        }
     }
 }
