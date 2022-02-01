57772a3f3a2b543722907fbfc9feab150a4779e8
Avery Dinh <averydinh1996@gmail.com>
2022-02-01 10:34:00 -0800

    Added /// comments to HomeMenuItem.cs

--- Mine/Mine/Models/HomeMenuItem.cs
+++ Mine/Mine/Models/HomeMenuItem.cs
@@ -4,12 +4,19 @@ using System.Text;
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Enum class containing all currently supported menu option
+    /// </summary>
     public enum MenuItemType
     {
         Items,
         About,
         Game
     }
+
+    /// <summary>
+    /// Class to contain a menu option
+    /// </summary>
     public class HomeMenuItem
     {
         public MenuItemType Id { get; set; }
