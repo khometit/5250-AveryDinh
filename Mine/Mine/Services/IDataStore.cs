c5e8e70ec8d6e4d6659eeeffeb20f83e887ea507
Avery Dinh <averydinh1996@gmail.com>
2022-02-01 10:28:58 -0800

    Added /// and // comments to IDataStore.cs

--- Mine/Mine/Services/IDataStore.cs
+++ Mine/Mine/Services/IDataStore.cs
@@ -4,12 +4,45 @@ using System.Threading.Tasks;
 
 namespace Mine.Services
 {
+    /// <summary>
+    /// Interface for interactions with the data store
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IDataStore<T>
     {
+        /// <summary>
+        /// Create a new record
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         Task<bool> CreateAsync(T item);
+
+        /// <summary>
+        /// Update a record
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         Task<bool> UpdateAsync(T item);
+
+        /// <summary>
+        /// Delete a record from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<bool> DeleteAsync(string id);
+
+        /// <summary>
+        /// Retrieve a record from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<T> ReadAsync(string id);
+
+        /// <summary>
+        /// Retrieve all available records
+        /// </summary>
+        /// <param name="forceRefresh"></param>
+        /// <returns></returns>
         Task<IEnumerable<T>> IndexAsync(bool forceRefresh = false);
     }
 }
