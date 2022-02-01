ed74e54e1c5547a245096ae7613a6fead387a84f
Avery Dinh <averydinh1996@gmail.com>
2022-02-01 10:30:25 -0800

    Added /// and // comments to ItemReadViewModel.cs

--- Mine/Mine/ViewModels/ItemReadViewModel.cs
+++ Mine/Mine/ViewModels/ItemReadViewModel.cs
@@ -4,9 +4,17 @@ using Mine.Models;
 
 namespace Mine.ViewModels
 {
+    /// <summary>
+    /// View Model for Item Read page
+    /// </summary>
     public class ItemReadViewModel : BaseViewModel
     {
         public ItemModel Item { get; set; }
+
+        /// <summary>
+        /// Default constructor that takes an ItemModel, which defaults to null if nothing is passed
+        /// </summary>
+        /// <param name="item"></param>
         public ItemReadViewModel(ItemModel item = null)
         {
             Title = item?.Text;
