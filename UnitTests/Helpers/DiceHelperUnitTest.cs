00055ebdd5075b499f36d98cdbbf1929e22328ff
Avery Dinh <averydinh1996@gmail.com>
2022-02-04 15:56:08 -0800

    Added RollDice_Invalid to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -9,7 +9,19 @@ namespace UnitTests.Helpers
     /// </summary>
     public class DiceHelperUnitTest
     {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            //Arrange
 
+            //Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
          
     }
 }
