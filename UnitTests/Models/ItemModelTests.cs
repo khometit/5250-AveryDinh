56da2330f56f1f24130339d30cc3325730e44bb8
Avery Dinh <averydinh1996@gmail.com>
2022-02-04 10:34:51 -0800

    Added ItemModel_Get_Set UT to ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -5,6 +5,9 @@ namespace UnitTests.Models
 {
     public class ItemModelTests
     {
+        /// <summary>
+        /// Test constructor with default value
+        /// </summary>
         [Test]
         public void ItemModel_Constructor_Valid_Default_Should_Pass()
         {
@@ -12,10 +15,34 @@ namespace UnitTests.Models
 
             //Act
             var result = new ItemModel();
+
             //Reset
 
             //Assert
             Assert.IsNotNull(result);
+        }
+
+        /// <summary>
+        /// Test set and get methods with default values
+        /// </summary>
+        [Test]
+        public void ItemModel_Set_Get_Valid_Default_Should_Pass()
+        {
+            //Arrange
+
+            //Act
+            var result = new ItemModel();
+            result.Description = "Description";
+            result.Id = "ID";
+            result.Text = "Text";
+            result.Value = 1;
+            //Reset
+
+            //Assert
+            Assert.AreEqual("Description", result.Description);
+            Assert.AreEqual("ID", result.Id);
+            Assert.AreEqual("Text", result.Text);
+            Assert.AreEqual(1, result.Value);
         }
     }
 }
