b59cbb7fef5530b51942eddf1aad3798c86295a9
Avery Dinh <averydinh1996@gmail.com>
2022-02-04 10:01:18 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
+        {
+            //Arrange
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
