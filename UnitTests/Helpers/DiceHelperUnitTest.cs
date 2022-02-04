4edff6e178d831bf1032bdde5d6fc613ab2898a9
Avery Dinh <averydinh1996@gmail.com>
2022-02-04 15:55:11 -0800

    Added DiceHelperUnitTest.cs to UT project

---
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Mine.Models;
+using NUnit.Framework;
+using Mine.Helpers;
+
+namespace UnitTests.Helpers
+{
+    /// <summary>
+    /// Class containing Dice Helper Unit Tests
+    /// </summary>
+    public class DiceHelperUnitTest
+    {
+
+         
+    }
+}
